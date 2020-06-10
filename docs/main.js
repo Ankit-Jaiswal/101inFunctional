c58d85897dd7c94dc5f17e3a7bf63fe1766dea8f
Ankit-Jaiswal <akj24ankit@gmail.com>
2020-06-10 16:02:43 +0530

    corrected grammar and design

--- docs/main.js
+++ docs/main.js
@@ -5,11 +5,13 @@ function myFunction() {
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("banner").style.fontSize = "80%";
+    document.getElementById("banner").style.fontSize = "70%";
     document.getElementById("tagline").style.display = "none";
+    document.getElementById("banner").style.padding = "0px";
   } else {
     document.getElementById("banner").style.fontSize = "130%";
     document.getElementById("tagline").style.display = "inherit";
+    document.getElementById("banner").style.padding = "1% 0 0 0"
   }
   document.getElementById("progress-bar").style.width = scrolled + "%";
 }
